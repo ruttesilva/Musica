25e7b819344a6c0978445434d2449b02e68b30ab
Rutte <rosrosrutte@gmail.com>
2022-03-10 01:42:35 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Hey Jude - The Beatles");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Hey Jude, don't make it bad.");
+Console.WriteLine("Take a sad song and make it better.");
+Console.WriteLine("Remember to let her into your heart,");
+Console.WriteLine("Then you can start to make it better.");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Hey Jude, don't be afraid.");
+Console.WriteLine("You were made to go out and get her.");
+Console.WriteLine("The minute you let her under your skin,");
+Console.WriteLine("Then you begin to make it better.");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("And anytime you feel the pain, hey Jude, refrain,");
+Console.WriteLine("Don't carry the world upon your shoulders.");
+Console.WriteLine("For well you know that it's a fool who plays it cool");
+Console.WriteLine("By making his world a little colder.");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Hey Jude, don't let me down.");
+Console.WriteLine("You have found her, now go and get her.");
+Console.WriteLine("Remember to let her into your heart,");
+Console.WriteLine("Then you can start to make it better.");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("So let it out and let it in, hey Jude, begin,");
+Console.WriteLine("You're waiting for someone to perform with.");
+Console.WriteLine("And don't you know that it's just you, hey Jude, you'll do,");
+Console.WriteLine("The movement you need is on your shoulder.");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Hey Jude, don't make it bad.");
+Console.WriteLine("Take a sad song and make it better.");
+Console.WriteLine("Remember to let her under your skin,");
+Console.WriteLine("Then you'll begin to make it");
+Console.WriteLine("Better better better better better better, oh.");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Na na na nananana, nannana, hey Jude... ");
+Console.WriteLine();
